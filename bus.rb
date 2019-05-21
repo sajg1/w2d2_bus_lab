74090fc30b4b9dea21b98f0d7a1668eaf5fa8f39
Steve Gossip <stephengossip@gmail.com>
2019-05-21 15:28:08 +0100

    Trying to add passenger array and functionality

--- bus.rb
+++ bus.rb
@@ -5,11 +5,20 @@ class Bus
   def initialize(route_number, destination)
     @route_number = route_number
     @destination = destination
+    @passengers = []
   end
 
   def drive(route_number)
     return "Brum brum"
   end
 
-  
+  def passengers()
+    return @passengers.count
+  end
+
+  def add_passenger(name)
+    @passengers << name.person_name
+  end
+
+
 end
--- specs/bus_spec.rb
+++ specs/bus_spec.rb
@@ -1,12 +1,14 @@
 require("minitest/autorun")
 require("minitest/rg")
 require_relative("../bus")
+require_relative("../person")
 
 
 class BusTest < MiniTest::Test
 
   def setup
-    @bus = Bus.new("10", "Bonaly")
+    @person_1 = Person.new("Simon", "23")
+    @bus = Bus.new("10", "Bonaly" )
   end
 
   def test_route_number
@@ -19,6 +21,14 @@ class BusTest < MiniTest::Test
 
   def test_drive
     assert_equal("Brum brum", @bus.drive("10"))
+  end
+
+  def test_total_passengers
+    assert_equal(0, @bus.passengers())
+  end
+
+  def test_add_passenger
+    assert_equal(1, @bus.passengers())
   end
 
 end
